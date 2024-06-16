c4ae972157e0d6201affb35e3c0c10c960384aee
Debasis08 <patrodebasis8@gmail.com>
2024-06-16 19:29:57 +0530

    Updated other files

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,9 +1,10 @@
-const Page = () => {
-    return (
-        <div>
-            <h1 className="text-white">Analytics Page</h1>
-        </div>
-    );
+import { analytics } from "@/utils/analytics";
+
+const Page = async () => {
+
+    const pageview = await analytics.retrieve("pageview", "15/06/2024")
+
+    return <pre className="text-white">{JSON.stringify(pageview)}</pre>;
 }
 
 export default Page;
